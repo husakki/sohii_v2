012805f6a997b876e04f33b922c7a7f4735860a8
husakki <81704101+husakki@users.noreply.github.com>
2021-10-23 19:49:02 +0200

    setting the theme

---
+++ lib/constants.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+ThemeData mainTheme = ThemeData(
+  fontFamily: 'Sora',
+  scaffoldBackgroundColor: const Color(0xff131313),
+  textTheme: const TextTheme(
+    bodyText1: TextStyle(),
+    bodyText2: TextStyle(),
+  ).apply(
+    bodyColor: Colors.white,
+  ),
+  colorScheme: ColorScheme.fromSwatch(),
+);
