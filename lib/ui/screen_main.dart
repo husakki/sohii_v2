3ece9e01d07fb93f709e450b053cb1c43ada1934
husakki <81704101+husakki@users.noreply.github.com>
2021-10-24 11:07:38 +0200

    WiP Parallex effect | learning Flutter Layout

--- lib/ui/screen_main.dart
+++ lib/ui/screen_main.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
 
-class ScreenMain extends StatelessWidget {
+class ScreenMain extends StatefulWidget {
   const ScreenMain({Key? key}) : super(key: key);
 
   @override
+  State<ScreenMain> createState() => _ScreenMainState();
+}
+
+class _ScreenMainState extends State<ScreenMain> {
+  double get screenHeight => MediaQuery.of(context).size.height;
+
+  double get screenWidth => MediaQuery.of(context).size.width;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("Moin :)"),
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: const [
+          FittedBox(
+            fit: BoxFit.fitWidth,
+            child: Image(
+              image: AssetImage("assets/images/Tshirt_Parallex.png"),
+            ),
+          ),
+          Center(
+            child: Image(
+              image: AssetImage("assets/images/Sohii_Logo.png"),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
