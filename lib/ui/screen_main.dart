38af5a509a839c2deaa98c52dbe351887054b03d
husakki <81704101+husakki@users.noreply.github.com>
2021-10-24 11:52:09 +0200

    WiP Alignment of the Widgets

--- lib/ui/screen_main.dart
+++ lib/ui/screen_main.dart
@@ -16,15 +16,26 @@ class _ScreenMainState extends State<ScreenMain> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
+        alignment: Alignment.topCenter,
         fit: StackFit.expand,
-        children: const [
-          FittedBox(
-            fit: BoxFit.fitWidth,
-            child: Image(
-              image: AssetImage("assets/images/Tshirt_Parallex.png"),
+        children: [
+          Container(
+            margin: EdgeInsets.only(
+              //top: screenHeight * 0.7,
+              left: screenWidth * 0.02,
+              right: screenWidth * 0.02,
+            ),
+            child: const FittedBox(
+              fit: BoxFit.fitWidth,
+              child: Image(
+                image: AssetImage("assets/images/Tshirt_Parallex.png"),
+              ),
             ),
           ),
-          Center(
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
             child: Image(
               image: AssetImage("assets/images/Sohii_Logo.png"),
             ),
