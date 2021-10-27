ad195d59440dd094ca787d95bb9071a1ad4615dc
husakki <hueschromo@gmail.com>
2021-10-27 18:02:52 +0200

    layout
    trying to figure out how to correctly do the layout of the widgets. >.<

--- lib/ui/screen_main.dart
+++ lib/ui/screen_main.dart
@@ -19,24 +19,15 @@ class _ScreenMainState extends State<ScreenMain> {
         alignment: Alignment.topCenter,
         fit: StackFit.expand,
         children: [
-          Container(
-            margin: EdgeInsets.only(
-              //top: screenHeight * 0.7,
-              left: screenWidth * 0.02,
-              right: screenWidth * 0.02,
-            ),
-            child: const FittedBox(
-              fit: BoxFit.fitWidth,
-              child: Image(
-                image: AssetImage("assets/images/Tshirt_Parallex.png"),
-              ),
+          Positioned(
+            height: screenHeight,
+            child: const Image(
+              image: AssetImage("assets/images/Tshirt_Parallex.png"),
             ),
           ),
           Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            child: Image(
+            top: screenHeight * 0.05,
+            child: const Image(
               image: AssetImage("assets/images/Sohii_Logo.png"),
             ),
           ),
