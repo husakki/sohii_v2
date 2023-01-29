49d82487ba931c15408ab3dd00b7fadaf4597ba5
husakki <hueschromo@gmail.com>
2023-01-29 18:47:40 +0100

    let it begin

--- lib/view/product_view.dart
+++ lib/view/product_view.dart
@@ -28,33 +28,24 @@ class _ProductViewState extends State<ProductView> {
 
     return FutureBuilder<List<Product>>(
       future: products.jsonProducts.value,
-      builder: (context, jsonContent) {
-        if (jsonContent.hasData) {
+      builder: (context, products) {
+        if (products.hasData) {
           return ListView.builder(
             // TODO need to make a getx object to share the controller with the screen
             //TODO main and make it react to it.
             // controller: ,
-            itemCount: jsonContent.data!.length,
+            itemCount: products.data!.length,
             itemBuilder: (context, index) {
               return Column(
                 children: [
-                  //TODO need to be moved above the listview builder
-                  // Container(
-                  //   height: screenHeight,
-                  // ),
-                  Container(
-                    // height: 500,
-                    child: Text("${jsonContent.data![index].product}"),
-                  ),
+                  Text(products.data![index].product),
                 ],
               );
             },
           );
-        } else if (jsonContent.hasError) {
-          return Text("${jsonContent.error}");
+        } else if (products.hasError) {
+          return Text("${products.error}");
         }
-
-        // By default, show a loading spinner.
         return const CircularProgressIndicator();
       },
     );
--- lib/view/screen_main.dart
+++ lib/view/screen_main.dart
@@ -38,25 +38,23 @@ class _ScreenMainState extends State<ScreenMain> {
         alignment: Alignment.center,
         clipBehavior: Clip.none,
         children: [
-          Positioned(
-            top: screenHeight * 0.2,
-            width: screenWidth,
-            child: const Image(
-              fit: BoxFit.fitWidth,
-              image: AssetImage("assets/images/Tshirt_Parallex.png"),
-            ),
-          ),
-          Positioned(
-            top: screenHeight * -0.07,
-            child: Image(
-              height: screenHeight * 0.6,
-              width: screenWidth * 0.75,
-              image: const AssetImage("assets/images/Sohii_Logo.png"),
-            ),
-          ),
-          const Scrollbar(
-            child: ProductView(),
-          ),
+          // Positioned(
+          //   top: screenHeight * 0.2,
+          //   width: screenWidth,
+          //   child: const Image(
+          //     fit: BoxFit.fitWidth,
+          //     image: AssetImage("assets/images/Tshirt_Parallex.png"),
+          //   ),
+          // ),
+          // Positioned(
+          //   top: screenHeight * -0.07,
+          //   child: Image(
+          //     height: screenHeight * 0.6,
+          //     width: screenWidth * 0.75,
+          //     image: const AssetImage("assets/images/Sohii_Logo.png"),
+          //   ),
+          // ),
+          ProductView(),
         ],
       ),
     );
